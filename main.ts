9262e14df38ec0dae80e1fb5c96f16e6ce35fc71
Juan-789 <79994699+Juan-789@users.noreply.github.com>
2021-03-31 00:27:57 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,16 +12,18 @@ input.onButtonPressed(Button.AB, function () {
 })
 input.onButtonPressed(Button.B, function () {
     if (case_test == 1) {
-        pins.digitalWritePin(DigitalPin.P0, 0)
-        pins.digitalWritePin(DigitalPin.P2, 1)
-        basic.pause(5000)
-        pins.digitalWritePin(DigitalPin.P1, 1)
-        pins.digitalWritePin(DigitalPin.P2, 0)
-        basic.pause(5000)
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        pins.digitalWritePin(DigitalPin.P1, 0)
-        basic.showString("Walk")
-        basic.pause(5000)
+        while (true) {
+            pins.digitalWritePin(DigitalPin.P0, 0)
+            pins.digitalWritePin(DigitalPin.P2, 1)
+            basic.pause(5000)
+            pins.digitalWritePin(DigitalPin.P1, 1)
+            pins.digitalWritePin(DigitalPin.P2, 0)
+            basic.pause(5000)
+            pins.digitalWritePin(DigitalPin.P0, 1)
+            pins.digitalWritePin(DigitalPin.P1, 0)
+            basic.showString("Walk")
+            basic.pause(5000)
+        }
     } else if (case_test == 2) {
         while (true) {
             pins.digitalWritePin(DigitalPin.P1, 1)
