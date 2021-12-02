da2fc5aece758ba7dbeeb1a5e058d960ff4a454d
Juan-789 <79994699+Juan-789@users.noreply.github.com>
2021-12-02 13:34:33 -0500

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+def Green_Light():
+    global range2
+    range2 = strip.range(0, 1)
+    strip.show_color(neopixel.colors(NeoPixelColors.BLACK))
+    range2 = strip.range(1, 1)
+    strip.show_color(neopixel.colors(NeoPixelColors.BLACK))
+    range2 = strip.range(2, 1)
+    strip.show_color(neopixel.colors(NeoPixelColors.GREEN))
 """
 
 P0=green
@@ -7,6 +15,34 @@ p1=yellow
 p2=red
 
 """
+def Red_Green_Yellow_Test_case():
+    global index
+    strip.set_brightness(20)
+    Green_Light()
+    basic.show_string("Walk")
+    basic.show_leds("""
+        . . # . .
+                . # . . .
+                # # # # #
+                . # . . .
+                . . # . .
+    """)
+    while index > 0:
+        index += -1
+        basic.show_number(index)
+    basic.clear_screen()
+    basic.show_leds("""
+        # . . . #
+                . # . # .
+                . . # . .
+                . # . # .
+                # . . . #
+    """)
+    Yellow_Light()
+    basic.pause(1000)
+    index = 10
+    Red_Light()
+# This blocks sets which case test are we going to use
 
 def on_button_pressed_a():
     global case_test
@@ -15,44 +51,46 @@ def on_button_pressed_a():
         case_test = 0
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
+def Red_Light():
+    global range2
+    range2 = strip.range(0, 1)
+    strip.show_color(neopixel.colors(NeoPixelColors.RED))
+    range2 = strip.range(1, 1)
+    strip.show_color(neopixel.colors(NeoPixelColors.BLACK))
+    range2 = strip.range(2, 1)
+    strip.show_color(neopixel.colors(NeoPixelColors.BLACK))
+# Resets the micro bit and the lights
+
 def on_button_pressed_ab():
-    pins.digital_write_pin(DigitalPin.P0, 0)
-    pins.digital_write_pin(DigitalPin.P1, 0)
-    pins.digital_write_pin(DigitalPin.P2, 0)
     basic.clear_screen()
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
+# Executes the test case that you chose
+
 def on_button_pressed_b():
+    # This while loop executes the first test case which shows at first the green light, then the left turn, then the Walk sign, and then the countdown for the pedestrians
     while case_test == 1:
-        pins.digital_write_pin(DigitalPin.P0, 1)
-        basic.show_string("Walk")
-        basic.show_leds("""
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            """)
-        basic.pause(20000)
-        basic.pause(5000)
-        pins.digital_write_pin(DigitalPin.P0, 0)
-        pins.digital_write_pin(DigitalPin.P1, 1)
-        basic.pause(1000)
-        pins.digital_write_pin(DigitalPin.P1, 0)
-        pins.digital_write_pin(DigitalPin.P2, 1)
-        basic.pause(10000)
-        pins.digital_write_pin(DigitalPin.P2, 0)
-    while case_test == 2:
-        pins.digital_write_pin(DigitalPin.P1, 1)
-        basic.pause(500)
-        pins.digital_write_pin(DigitalPin.P1, 0)
-        basic.pause(500)
-    while case_test == 3:
-        pins.digital_write_pin(DigitalPin.P2, 1)
-        basic.pause(500)
-        pins.digital_write_pin(DigitalPin.P2, 0)
-        basic.pause(500)
+        Red_Green_Yellow_Test_case()
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
+def Yellow_Light():
+    global range2
+    range2 = strip.range(0, 1)
+    strip.show_color(neopixel.colors(NeoPixelColors.BLACK))
+    range2 = strip.range(1, 1)
+    strip.show_color(neopixel.colors(NeoPixelColors.YELLOW))
+    range2 = strip.range(2, 1)
+    strip.show_color(neopixel.colors(NeoPixelColors.BLACK))
+# sets the case test to 0, and sets index, which is the variable used for the countdown as 10
+index = 0
 case_test = 0
+range2: neopixel.Strip = None
+strip: neopixel.Strip = None
+strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+range2 = strip.range(0, 3)
 case_test = 0
+index = 10
+
+def on_forever():
+    pass
+basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,75 +1,121 @@
 /**
+ * do the
+ */
+function Green_Light () {
+    range = strip.range(0, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+    range = strip.range(1, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+    range = strip.range(2, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Green))
+}
+function Red_Green_Yellow_Test_case () {
+    strip.setBrightness(20)
+    Green_Light()
+    basic.showString("Walk")
+    while (index > 0) {
+        index += -1
+        basic.showNumber(index)
+    }
+    music.stopMelody(MelodyStopOptions.All)
+    basic.clearScreen()
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    Yellow_Light()
+    basic.pause(1000)
+    Red_Light()
+    basic.pause(10000)
+    index = 10
+}
+// This blocks sets which case test are we going to use
+input.onButtonPressed(Button.A, function () {
+    Deaf_Red_Green_Yellow_Test_case2()
+    case_test += 1
+    // This while loop executes the first test case which shows at first the green light, then the left turn, then the Walk sign, and then the countdown for the pedestrians
+    while (case_test == 2) {
+        Red_Green_Yellow_Test_case()
+    }
+})
+function Red_Light () {
+    range = strip.range(0, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Red))
+    range = strip.range(1, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+    range = strip.range(2, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+}
+// Resets the micro bit and the lights
+input.onButtonPressed(Button.AB, function () {
+	
+})
+// Executes the test case that you chose
+input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
+})
+/**
  * P0=green
  * 
  * p1=yellow
  * 
  * p2=red
  */
-// This blocks sets which case test are we going to use
-input.onButtonPressed(Button.A, function () {
-    case_test += 1
-    if (case_test > 3) {
-        case_test = 0
-    }
-})
-// Resets the micro bit and the lights
-input.onButtonPressed(Button.AB, function () {
-    pins.digitalWritePin(DigitalPin.P0, 0)
-    pins.digitalWritePin(DigitalPin.P1, 0)
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    basic.clearScreen()
-})
-// Executes the test case that you chose
-input.onButtonPressed(Button.B, function () {
-    // This while loop executes the first test case which shows at first the green light, then the left turn, then the Walk sign, and then the countdown for the pedestrians
-    while (case_test == 1) {
-        pins.digitalWritePin(DigitalPin.P0, 1)
-        basic.showLeds(`
-            . . # . .
-            . # . . .
-            # # # # #
-            . # . . .
-            . . # . .
-            `)
-        basic.showString("Walk")
-        while (index > 0) {
-            index += -1
-            basic.showNumber(index)
+function Deaf_Red_Green_Yellow_Test_case2 () {
+    strip.setBrightness(20)
+    Green_Light()
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `)
+    while (index > 0) {
+        index += -1
+        basic.showNumber(index)
+        while (index > 5) {
+            music.playTone(659, music.beat(BeatFraction.Half))
+            music.playTone(554, music.beat(BeatFraction.Whole))
+            music.playTone(247, music.beat(BeatFraction.Whole))
+            music.playTone(440, music.beat(BeatFraction.Whole))
+            break;
         }
-        basic.clearScreen()
-        basic.showLeds(`
-            # . . . #
-            . # . # .
-            . . # . .
-            . # . # .
-            # . . . #
-            `)
-        pins.digitalWritePin(DigitalPin.P0, 0)
-        pins.digitalWritePin(DigitalPin.P1, 1)
-        basic.pause(1000)
-        pins.digitalWritePin(DigitalPin.P1, 0)
-        pins.digitalWritePin(DigitalPin.P2, 1)
-        basic.pause(10000)
-        pins.digitalWritePin(DigitalPin.P2, 0)
-        index = 10
     }
-    // Executes test case 2, this shows a flashing yellow light
-    while (case_test == 2) {
-        pins.digitalWritePin(DigitalPin.P1, 1)
-        basic.pause(500)
-        pins.digitalWritePin(DigitalPin.P1, 0)
-        basic.pause(500)
-    }
-    // Executes test case 3, shows a flashing red light
-    while (case_test == 3) {
-        pins.digitalWritePin(DigitalPin.P2, 1)
-        basic.pause(500)
-        pins.digitalWritePin(DigitalPin.P2, 0)
-        basic.pause(500)
-    }
-})
+    basic.clearScreen()
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    Yellow_Light()
+    basic.pause(1000)
+    Red_Light()
+    basic.pause(10000)
+    index = 10
+}
+function Yellow_Light () {
+    range = strip.range(0, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+    range = strip.range(1, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    range = strip.range(2, 1)
+    range.showColor(neopixel.colors(NeoPixelColors.Black))
+}
 // sets the case test to 0, and sets index, which is the variable used for the countdown as 10
 let index = 0
 let case_test = 0
+let range: neopixel.Strip = null
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P16, 3, NeoPixelMode.RGB)
+range = strip.range(0, 3)
 case_test = 0
 index = 10
+basic.forever(function () {
+	
+})
